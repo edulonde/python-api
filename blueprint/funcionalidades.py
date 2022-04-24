fad04be3932f1628b51753e6b8d0c4c1247f4c45
edulonde <edulonde@gmail.com>
2022-04-24 12:20:29 -0300

    +

--- blueprint/funcionalidades.py
+++ blueprint/funcionalidades.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Blueprint, render_template
 
 blueprint = Blueprint("funcionalidades", __name__)
@@ -5,4 +7,6 @@ blueprint = Blueprint("funcionalidades", __name__)
 
 @blueprint.route('/funcionalidades')
 def show_page_funcionalidades():
-    return render_template('funcionalidades.html')
+    hora = datetime.datetime.now()
+    return render_template('funcionalidades.html', hora=hora)
+
